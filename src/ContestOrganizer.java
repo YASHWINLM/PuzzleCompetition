d460b456f5c1456c374eab6902ea0381adb0e974
YASHWINLM <league@WTS14.attlocal.net>
2018-02-25 12:57:23 -0800

    Continue location puzzle

--- src/ContestOrganizer.java
+++ src/ContestOrganizer.java
@@ -1,40 +1,92 @@
+import java.util.Random;
 
 public class ContestOrganizer {
-	/** the number of rows of contestant */
-	public static final int NUM_ROWS = 10;
-	
-	/** the number of columns of contestant */
-	public static final int CONTESTANTS_PER_ROW = 10;
-	
-	/** the two-dimensional array of contestant */
-	private Contestant[][] contestants; 
-	
-	/** Sorts arr in increasing order by score 
-	 * Postcondition:
-	 * - arr sorted in increasing order by score.
-	 * - Location of each contestant correctly updated such that
-	 * column number matches contestant's position in arr.
-	 * @param arr the array to be sorted.*/
-	public void sort(Contestant[] arr){
-		/* code goes here */
-	}
-	
-	/** Sorts each row of contestants into increasing order by score.
-	 * Postcondition: Contestant with highest score in row[k] is
-	 * 				  in the rightmost column of row[k], 0 <= k < NUM_ROWS */
-	public void sortAllRows(){
-		/* code goes here */ 
+
+	public static void main(String[] args) {
+		ContestOrganizer co = new ContestOrganizer();
+		co.createContestants();
+		co.sortAllRows();
+		co.printScores();
 	}
 
-	/** Returns name of contestant with highest score
-	 * Precondition:
-	 * - Contestants have not been sorted by score.
-	 * - Top score is unique
-	 * - Only one contestant has the highest score.
-	 * @return name of contestant with highest score. */
-	public String findWinnerName(){
+	/** the number of rows of contestant */
+	public static final int NUM_ROWS = 10;
+
+	/** the number of columns of contestant */
+	public static final int CONTESTANTS_PER_ROW = 10;
+
+	/** the two-dimensional array of contestant */
+	private Contestant[][] contestants;
+
+	/**
+	 * Sorts arr in increasing order by score Postcondition: - arr sorted in
+	 * increasing order by score. - Location of each contestant correctly updated
+	 * such that column number matches contestant's position in arr.
+	 * 
+	 * @param arr
+	 *            the array to be sorted.
+	 */
+	public void sort(Contestant[] arr) {
+		boolean swap = true;
+		while (swap) {
+			swap = false;
+			for (int i = 0; i < arr.length - 1; i++) {
+				if (arr[i].getScore() > arr[i + 1].getScore()) {
+					int x = arr[i].getScore();
+					arr[i].setScore(arr[i + 1].getScore());
+					arr[i + 1].setScore(x);
+					swap = true;
+				}
+			}
+		}
+	}
+
+	public void createContestants() {
+		Contestant[][] newContestants = new Contestant[10][10];
+		int contestantNumber = 0;
+		for (int i = 0; i < 10; i++) {
+			for (int j = 0; j < 10; j++) {
+				Contestant temp = new Contestant(i, j);
+				temp.setScore(new Random().nextInt(200));
+				// temp.setName("Person " + contestantNumber++);
+				newContestants[i][j] = temp;
+			}
+		}
+		this.contestants = newContestants;
+	}
+
+	/**
+	 * Sorts each row of contestants into increasing order by score. Postcondition:
+	 * Contestant with highest score in row[k] is in the rightmost column of row[k],
+	 * 0 <= k < NUM_ROWS
+	 */
+	public void sortAllRows() {
+		for (Contestant[] contestants2 : contestants) {
+			sort(contestants2);
+
+		}
+	}
+
+	public void printScores() {
+		for (int i = 0; i < contestants.length; i++) {
+			for (int j = 0; j < contestants[i].length; j++) {
+				System.out.println(contestants[i][j].getName() + ": " + contestants[i][j].getScore());
+			}
+			System.out.println("\n\n");
+		}
+
+	}
+
+	/**
+	 * Returns name of contestant with highest score Precondition: - Contestants
+	 * have not been sorted by score. - Top score is unique - Only one contestant
+	 * has the highest score.
+	 * 
+	 * @return name of contestant with highest score.
+	 */
+	public String findWinnerName() {
 		/* code goes here */
-		
+
 		return "";
 	}
 }
--- src/Contestant.java
+++ src/Contestant.java
@@ -3,36 +3,50 @@ public class Contestant {
 	private String name;
 	private int score;
 	private Location loc;
-	
+
+	public static void main(String[] args) {
+		Contestant c = new Contestant(10, 0);
+
+	}
+
 	/** @return the name of this contestant */
 	public String getName() {
 		return name;
 	}
-	
+
 	/** @return the score of this contestant */
 	public int getScore() {
 		return score;
 	}
-	
+
 	/** @return the location of this contestant */
 	public Location getLoc() {
 		return loc;
 	}
-	
-	/** @param int for updated score */
-	public void setScore(int newScore){
+
+	/**
+	 * @param int
+	 *            for updated score
+	 */
+	public void setScore(int newScore) {
 		score = newScore;
 	}
-	
-	/** Changes the location of this contestant to a new row
-	 *  and column.
-	 *  @param newRow the new row
-	 *  @param newCol the new column */
-	public void updateLocation(int newRow, int newCol){
-		/* code goes here */
+
+	/**
+	 * Changes the location of this contestant to a new row and column.
+	 * 
+	 * @param newRow
+	 *            the new row
+	 * @param newCol
+	 *            the new column
+	 */
+	public void updateLocation(int newRow, int newCol) {
+
+		loc = new Location(newRow, newCol);
+
 	}
-	
-	public Contestant(int r, int c){
+
+	public Contestant(int r, int c) {
 		loc = new Location(r, c);
 	}
 }
